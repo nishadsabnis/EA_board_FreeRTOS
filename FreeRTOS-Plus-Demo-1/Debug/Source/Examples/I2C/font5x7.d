8523bdd31eed7061f6e70030f34839436e5a1c08
Nishad Sabnis <nishadsabnis@gmail.com>
2014-10-21 22:29:04 +0530

    Removed Debug sources

--- FreeRTOS-Plus-Demo-1/Debug/Source/Examples/I2C/font5x7.d
+++
@@ -1,4 +0,0 @@
-Source/Examples/I2C/font5x7.d: ../Source/Examples/I2C/font5x7.c \
- /home/nishad/fbaseboard_workspace/FreeRTOS-Plus-Demo-1/Source/Examples/Include/font_macro.h
-
-/home/nishad/fbaseboard_workspace/FreeRTOS-Plus-Demo-1/Source/Examples/Include/font_macro.h:
--- FreeRTOS-Plus-Demo-1/Debug/Source/cr_startup_lpc176x.d
+++
@@ -1,4 +0,0 @@
-Source/cr_startup_lpc176x.d: ../Source/cr_startup_lpc176x.c \
- /home/nishad/fbaseboard_workspace/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h
-
-/home/nishad/fbaseboard_workspace/CMSISv2p00_LPC17xx/inc/system_LPC17xx.h:
